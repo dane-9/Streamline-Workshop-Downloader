847839103d9c4f4d4220c9e93c0d77f3286a8dd5
dane-9 <74832166+dane-9@users.noreply.github.com>
2025-04-17 19:57:34 +0200

    Version change from '1.2.0' to '1.3.0'

--- downloader.py
+++ downloader.py
@@ -40,7 +40,7 @@ from PySide6.QtGui import (
     QColor, QPixmap, QPolygon, QFontMetrics, QActionGroup, QPalette
 )
 
-current_version = "1.2.0"
+current_version = "1.3.0"
 
 DEFAULT_SETTINGS = {
     "current_theme": "Dark",
